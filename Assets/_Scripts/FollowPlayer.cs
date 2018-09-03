b03de49cacd5ecf8ef6e2de324f8947b9d8b57d2
tylcos <tylcos@users.noreply.github.com>
2018-09-02 21:13:22 -0400

    Initial Project
    Create 2d Project with a background and player.

---
+++ Assets/_Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+	
+
+
+	void Update()
+	{
+        transform.RotateAround(player.position, Vector3.forward, 0);
+	}
+}
