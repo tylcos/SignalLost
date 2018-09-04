f91f0f9194ece6c0819033b2c3d10da1ada3479a
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-09-04 14:47:18 -0400

    Fixed projectile orientation to match where weapon is pointing

--- Assets/_Scripts/Firing.cs
+++ Assets/_Scripts/Firing.cs
@@ -7,6 +7,8 @@ public class Firing : MonoBehaviour
     public WeaponManager weaponManager;
     public Transform bulletSpawnPoint;
 
+    public CharacterController cc;
+
 
 
     private float timeLastFired;
@@ -22,14 +24,16 @@ public class Firing : MonoBehaviour
             GameObject bullet = Instantiate(
                 weaponManager.weapon.bullet,
                 bulletSpawnPoint.position,
-                transform.rotation);
+                bulletSpawnPoint.rotation);
 
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
 
-
+            Debug.Log(bulletSpawnPoint.rotation.eulerAngles);
 
             // Need to use atan instead of mouseDirection
-            rb.velocity = MouseWrapper.GetMouseDirection() * weaponManager.weapon.bulletSpeed;
+            float angle = bulletSpawnPoint.rotation.eulerAngles.z * Mathf.Deg2Rad;
+            Vector2 playerVelocity = (Vector2)(cc.velocity * Time.deltaTime);
+            rb.velocity = (new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) - playerVelocity) * weaponManager.weapon.bulletSpeed;
 
             Destroy(bullet, 2.0f);
         }
