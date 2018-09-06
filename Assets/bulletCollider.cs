bbdfccfb6440c6a5e45bdfe00212f0e9ec4646a1
Allen <loftedspy@gmail.com>
2018-09-06 14:42:16 -0400

    Bullet Collision and shooting
    bullet now collide with walls and the gun has been moved so the bullet doesn't hit the player

---
+++ Assets/bulletCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletCollider : MonoBehaviour {
+
+    private float time;
+	// Use this for initialization
+	void Start () {
+        time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (Time.time - time >= 2.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(gameObject);
+    }
+}
