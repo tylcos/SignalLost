1fb1ba5af2588554b653df61782706b36d273269
tylcos <tylcos@users.noreply.github.com>
2018-09-10 18:43:07 -0400

    Revert

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,27 +5,53 @@
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
 public class Weapon : ScriptableObject 
 {
+    [Tooltip("The ingame name of the weapon.")]
     public new string name;
+    [Tooltip("A description of the weapob.")]
     public string description;
 
+
+
+    [Space(20)]
+    [Tooltip("The sprite for the weapon.")]
     public Sprite weaponSprite;
+    [Tooltip("The sprite for the projectile fired.")]
     public GameObject bullet;
 
+
+
+    [Space(20)]
+    [Tooltip("The damage done to enemies when hit.")]
     public double damage;
+    [Tooltip("The speed of the bullet in units per second.")]
     public float bulletSpeed;
 
-<<<<<<< HEAD
-    public float fireRate;
-=======
+
+
+    [Space(20)]
+    [Tooltip("The time in seconds inbetween shots.")]
     public float cycleTime;
->>>>>>> parent of 4b21d91... Added random inaccuracy for weapons and improved the Weapon object
+    [Tooltip("The number of bullets in each clip.")]
+    public float clipSize;
+    [Tooltip("The time in seconds inbetween reloads.")]
     public float reloadTime;
 
-    public int numberOfProjectiles;
-    public float inaccuracy; // Angle of inaccuracy measured
 
-    public float range;
+
+    [Space(20)]
+    [Tooltip("The total number of projectiles fired per shot.")]
+    public int numberOfProjectiles;
+    [Range(0f, 90f)]
+    [Tooltip("The cone of inaccuracy in degrees.")]
+    public float inaccuracy;
+    [Tooltip("How long in seconds for the projectiles to stay alive for.")]
+    public float bulletLifeTime;
+    [Tooltip("Does the bullet continue after hitting an enemy.")]
     public bool penetrates;
 
+
+
+    [Space(20)]
+    [Tooltip("A position offset from the Weapon gameObject to spawn bullets at.")]
     public Vector3 bulletSpawnOffset;
 }
