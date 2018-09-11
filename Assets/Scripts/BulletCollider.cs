929b458ffe179b3ac7513f02b00d74a3653789b1
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-09-11 14:27:34 -0400

    Renamed bulletCollider to pascal case

---
+++ Assets/Scripts/BulletCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BulletCollider : MonoBehaviour
+{
+    [HideInInspector]
+    public float lifeTime;
+
+
+
+    private float startTime;
+
+
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+
+	
+	void LateUpdate()
+    {
+		if (Time.time - startTime >= lifeTime)
+            Destroy(gameObject);
+	}
+
+    void OnCollisionEnter2D(Collision2D collEvent)
+    {
+        Destroy(gameObject);
+    }
+}
