0ca302c0e46342ac2b4e18d6cf4bba8f3a164f0d
Allen <loftedspy@gmail.com>
2018-09-11 15:46:40 -0400

    wahts happening

---
+++ Assets/Scripts/BulletCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class bulletCollider : MonoBehaviour
+{
+    public float lifeTime;
+
+
+
+    private float time;
+
+
+
+    void Start()
+    {
+        time = Time.time;
+
+    }
+
+
+	
+	void LateUpdate()
+    {
+		if (Time.time - time >= lifeTime)
+            Destroy(gameObject);
+	}
+
+    void OnCollisionEnter2D(Collision2D collEvent)
+    {
+            Destroy(gameObject);
+    }
+}
