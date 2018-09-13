4d65409b83e28ed728ee26b21d2c28c9c96effd7
Allen <loftedspy@gmail.com>
2018-09-13 15:01:54 -0400

    fix

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -63,16 +63,6 @@ public class EnemyController : MonoBehaviour
         }
         else
             rb2d.velocity = Vector2.zero;
-
-<<<<<<< HEAD
-        /*if(inStunAnimation && Time.time - stunStart >= stunAnimationLength)
-=======
-        if (inStunAnimation && Time.time - stunStart >= stunAnimationLength)
->>>>>>> 2ac65282cdaae3202f4e910b4aa7f30dd5e7cac4
-        {
-            rb2d.velocity = Vector2.zero;
-            inStunAnimation = false;
-        }*/
     }
 
 
