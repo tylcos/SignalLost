42e7aba0ee4cb42a3dc09798121499e1666a0ec6
Allen <loftedspy@gmail.com>
2018-09-13 15:00:58 -0400

    Enemy movement uses moveposition now
    Enemy knockback is sudden now. Needs smoothing

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,14 +13,14 @@ public class EnemyController : MonoBehaviour
 
 
 
-    private static float stunAnimationLength = 0.1f;
-    private const float knockbackVelocity = 30;
+    //private static float stunAnimationLength = 0.1f;
+    private const float knockbackDistance = 2;
 
     private GameObject target = null;
     private float internalSpeed;
     private float internalAggroRange;
     private bool stunned;
-    private bool inStunAnimation;
+    //private bool inStunAnimation;
     private float stunStart;
     
 
@@ -57,14 +57,16 @@ public class EnemyController : MonoBehaviour
         if (target != null && !stunned)
         {
             Vector2 move = target.transform.position - gameObject.transform.position;
-            rb2d.velocity = move.normalized * internalSpeed * Time.deltaTime;
+            //rb2d.velocity = move.normalized * internalSpeed * Time.deltaTime;
+            Vector2 v = move.normalized * speed;
+            rb2d.MovePosition((Vector2)gameObject.transform.position + v);
         }
 
-        if(inStunAnimation && Time.time - stunStart >= stunAnimationLength)
+        /*if(inStunAnimation && Time.time - stunStart >= stunAnimationLength)
         {
             rb2d.velocity = Vector2.zero;
             inStunAnimation = false;
-        }
+        }*/
     }
 
 
@@ -73,10 +75,12 @@ public class EnemyController : MonoBehaviour
     {
         if (collEvent.gameObject.tag == "Player")
         {
-            rb2d.velocity = -rb2d.velocity.normalized * knockbackVelocity;
+            //rb2d.velocity = -rb2d.velocity.normalized * knockbackVelocity;
+            Vector2 normalToTarget = (target.transform.position - gameObject.transform.position).normalized;
+            rb2d.MovePosition((Vector2)gameObject.transform.position - (normalToTarget*knockbackDistance));
             stunStart = Time.time;
             stunned = true;
-            inStunAnimation = true;
+            //inStunAnimation = true;
         }
     }
 
