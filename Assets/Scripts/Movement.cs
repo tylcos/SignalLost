a6ae6117d2ed5008f324a49c6ced844bc3ecbd79
Allen <loftedspy@gmail.com>
2018-09-17 15:12:11 -0400

    Wall collision function aside from sprite clipping
    Todo: add enemy collision and fix clipping

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -26,6 +26,8 @@ public class Movement : MovementController
         //rb2d.velocity = move.normalized * internalSpeed * Time.deltaTime;
         Vector2 v = move.normalized * speed;
         //rb2d.MovePosition((Vector2)gameObject.transform.position + v);
+        Vector2 bcv = gameObject.GetComponent<BoxCollider2D>().size;
         Move(rb2d, gameObject.transform.position, v);
+        // for the origin. it needs to be the edge of the collider
     }
 }
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -16,14 +16,46 @@ public class MovementController : MonoBehaviour {
 
     protected void Move(Rigidbody2D rb2d, Vector2 origin, Vector2 moveVector)
     {
-        RaycastHit2D hit = Physics2D.Raycast(origin, moveVector.normalized, moveVector.magnitude, LayerMask.GetMask("Walls"));
-        if(hit.collider != null && (hit.fraction != 0 || hit.normal == -moveVector.normalized))
+        // recode this so that it first calculates how far the x goes then how far the y goes so that
+        // if there's a wall right in front of me but i'm pressing w and d i still move to the right, just not forward
+
+        float vert = 0f;
+        float horz = 0f;
+
+        RaycastHit2D hitVert = Physics2D.Raycast(origin, new Vector2(0, moveVector.y), moveVector.magnitude, LayerMask.GetMask("Walls"));
+        if (hitVert.collider != null)
         {
-            Vector2 subtractVector = (moveVector.normalized * hit.distance);
+            RaycastHit2D hitBack = Physics2D.Raycast(hitVert.point, -moveVector, moveVector.magnitude, LayerMask.GetMask("Player"));
+            vert = hitBack.distance;
+        }
+        else
+        {
+            vert = moveVector.y;
+        }
+        RaycastHit2D hitHorz = Physics2D.Raycast(origin, new Vector2(moveVector.x, 0), moveVector.magnitude, LayerMask.GetMask("Walls"));
+        if (hitHorz.collider != null)
+        {
+            RaycastHit2D hitBack = Physics2D.Raycast(hitHorz.point, -moveVector, moveVector.magnitude, LayerMask.GetMask("Player"));
+            horz = hitBack.distance;
+        }
+        else
+        {
+            horz = moveVector.x;
+        }
+
+        Vector2 finalMoveVector = new Vector2(horz, vert);
+        rb2d.MovePosition(finalMoveVector + origin);
+
+        /*
+        RaycastHit2D hit = Physics2D.Raycast(origin, moveVector, moveVector.magnitude, LayerMask.GetMask("Walls"));
+        if(hit.collider != null)
+        {
+            RaycastHit2D hitBack = Physics2D.Raycast(hit.point, -moveVector, moveVector.magnitude, LayerMask.GetMask("Player"));
+            Vector2 subtractVector = (moveVector.normalized * (hitBack.distance));
             rb2d.MovePosition(subtractVector + origin);
         } else
         {
             rb2d.MovePosition(moveVector + origin);
-        }
+        }*/
     }
 }
