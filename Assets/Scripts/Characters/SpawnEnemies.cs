1a5fc0395a794a31ec9af4223ac473b45f18b903
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-09-26 15:56:14 -0400

    Started work on the enemy spawner and changed the player to the new sprite.

---
+++ Assets/Scripts/Characters/SpawnEnemies.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public Tilemap map;
+
+    public Dictionary<string, GameObject> characters;
+
+    public int numberToSpawn;
+
+
+
+    void Start()
+    {
+        var tiles = map.GetTilesBlock(map.cellBounds).Where(t => t != null)
+            .OrderBy(x => Random.value).Take(numberToSpawn);
+
+
+
+        foreach (TileBase tile in tiles)
+        {
+            SpawnCharacter(characters[tile.name]);
+        }
+    }
+
+
+
+    public void SpawnCharacter(GameObject a)
+    {
+
+    }
+}
