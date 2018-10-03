c21cdfe69d8a64523529934d70bda0651150d5cd
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-10-03 14:59:30 -0400

    Revert "Revert "Completed character spawning by nesting gameobjects to specify spawnpoints""
    This reverts commit 64e14755d916d13fcbb3cac0f91f25f3a20e2b2e.

---
+++ Assets/Scripts/Rooms/Character Spawning/CharacterSpawner.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+
+
+public class CharacterSpawner : MonoBehaviour 
+{
+    public GameObject spawnCharacter;
+    public int numberToSpawn;
+}
--- Assets/Scripts/Rooms/RoomTemplate.cs
+++ Assets/Scripts/Rooms/RoomTemplate.cs
@@ -2,15 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomTemplate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+public class RoomTemplate : MonoBehaviour
+{
+    
 }
