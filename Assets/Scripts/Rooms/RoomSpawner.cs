f266a708168c9e32ab4e962c027bd2b1b9b3fcc9
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-10-16 15:59:27 -0400

    Continued work on the room spawner

--- Assets/Scripts/Rooms/RoomSpawner.cs
+++ Assets/Scripts/Rooms/RoomSpawner.cs
@@ -7,20 +7,20 @@ public class RoomSpawner : MonoBehaviour
 {
     public Room startRoom;
 
-    public int iterations = 2;
-    public int rooms = 4;
+    public int iterations = 3;
+    public int rooms = 6;
 
     
 
     public void SpawnRooms()
     {
-        Room baseRoom;
+        startRoom.Children.Add(new Room(Room.GetDirection()));
 
         for (int i = 0; i < iterations; i++)
         {
-            
+            int ratio = rooms / ((iterations - i) * startRoom.GetChildCountAtLevel(1 + iterations));
 
-            int ratio = rooms / (iterations * startRoom.Children.Capacity);
+            int numberOfRooms = x
         }
     }
 }
@@ -30,18 +30,72 @@ public class RoomSpawner : MonoBehaviour
 public class Room
 {
     public List<Room> Children = new List<Room>();
-    public Vector2 Position = new Vector2();
+    public Vector2Int Position;
 
 
+
+    public Room(Vector2Int position)
+    {
+        Position = position;
+    }
+    
+
+
+    public List<Room> GetChildAtLevel(int level)
+    {
+        List<Room> currentChildren = new List<Room>();
+
+        if (level > 1)
+        {
+            foreach (Room room in Children)
+                currentChildren.AddRange(room.GetChildAtLevel(level - 1));
+
+            return currentChildren;
+        }
+        else
+            return Children;
+    }
 
     public List<Room> GetLowestChildren()
     {
         List<Room> children = new List<Room>();
 
         foreach (Room child in Children)
-            children.AddRange(GetLowestChildren()):
+            children.AddRange(GetLowestChildren());
 
         return children;
+    }
+
+    
+
+    public int GetChildCountAtLevel(int level)
+    {
+        int count = 0;
+
+        if (level > 1)
+        {
+            foreach (Room room in Children)
+                count += room.GetChildCountAtLevel(level - 1);
+
+            return count;
+        }
+        else
+            return Children.Count;
+    }
+
+
+
+    public static Vector2Int GetDirection(int value)
+    {
+        Vector2Int[] directions = { new Vector2Int(1, 0), new Vector2Int(1, 1), new Vector2Int(0, 1), new Vector2Int(-1, 1),
+            new Vector2Int(-1, 0), new Vector2Int(-1, -1), new Vector2Int(0, -1), new Vector2Int(1, -1) };
+
+        return directions[value];
+    }
+
+    public static Vector2Int GetDirection()
+    {
+        return GetDirection(Random.Range(0, 8));
     }
 }
 
