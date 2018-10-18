35e5c399f924c16c835c8dd9a612ff4e868a7222
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-10-18 15:00:50 -0400

    Checks room position before spawning room

--- Assets/Scripts/Rooms/RoomSpawner.cs
+++ Assets/Scripts/Rooms/RoomSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 
 
@@ -26,7 +27,7 @@ public class RoomSpawner : MonoBehaviour
 
     public void SpawnRooms()
     {
-        startRoom.Children.Add(new Room(Room.GetDirection()));
+        startRoom.Children.Add(new Room(Room.GetDirectionVector()));
         
         for (int i = 0; i < iterations; i++)
         {
@@ -36,7 +37,17 @@ public class RoomSpawner : MonoBehaviour
             {
                 int numberOfRooms = GetRandom(ratio);
 
-                child.Children.Add(new Room(Room.GetDirection()));
+                for (int roomNumber = 0; roomNumber < numberOfRooms; roomNumber++)
+                {
+                    byte direction = (byte) Random.Range(0, 8);
+                    Vector2Int directionVector = Room.GetDirectionVector(direction);
+
+                    if (Room.takenPositions.Contains(directionVector))
+                        child.GetAvailableSpawnLocations().Take(1);
+
+                    child.Children.Add(new Room());
+                }
+                
 
                 Debug.Log(i + "    "  + child.Children.Count + "  Ratio :  " + ratio + "   Rooms :  " + numberOfRooms + "    Children " + startRoom.GetChildCountAtLevel(1 + i));
             }
@@ -63,6 +74,11 @@ public class Room
     public List<Room> Children = new List<Room>();
     public Vector2Int Position;
 
+    public static HashSet<Vector2Int> takenPositions = new HashSet<Vector2Int>();
+    public static Vector2Int[] Directions = 
+        { new Vector2Int(1, 0), new Vector2Int(1, 1), new Vector2Int(0, 1), new Vector2Int(-1, 1),
+            new Vector2Int(-1, 0), new Vector2Int(-1, -1), new Vector2Int(0, -1), new Vector2Int(1, -1) };
+
 
 
     public Room()
@@ -73,8 +89,23 @@ public class Room
     public Room(Vector2Int position)
     {
         Position = position;
+        takenPositions.Add(position);
     }
-    
+
+
+
+    public List<byte> GetAvailableSpawnLocations()
+    {
+        List<byte> validLocations = new List<byte>(8);
+
+        for (byte i = 0; i < 8; i++)
+            if (!takenPositions.Contains(Position + Directions[i]))
+                validLocations.Add(i);
+
+        return validLocations;
+    }
+
+
 
 
     public List<Room> GetChildrenAtLevel(int level)
@@ -121,17 +152,14 @@ public class Room
 
 
 
-    public static Vector2Int GetDirection(int value)
+    public static Vector2Int GetDirectionVector(byte value)
     {
-        Vector2Int[] directions = { new Vector2Int(1, 0), new Vector2Int(1, 1), new Vector2Int(0, 1), new Vector2Int(-1, 1),
-            new Vector2Int(-1, 0), new Vector2Int(-1, -1), new Vector2Int(0, -1), new Vector2Int(1, -1) };
-
-        return directions[value];
+        return Directions[value];
     }
 
-    public static Vector2Int GetDirection()
+    public static Vector2Int GetDirectionVector()
     {
-        return GetDirection(Random.Range(0, 8));
+        return GetDirectionVector((byte)Random.Range(0, 8));
     }
 }
 
@@ -139,11 +167,11 @@ public class Room
 
 public class DictonaryGrid
 {
-    private Dictionary<Vector2, byte> grid = new Dictionary<Vector2, byte>();
+    private Dictionary<Vector2Int, byte> grid = new Dictionary<Vector2Int, byte>();
 
     
 
-    public byte GetPosition(Vector2 pos)
+    public byte GetPosition(Vector2Int pos)
     {
         byte outValue = 0;
 
@@ -154,8 +182,24 @@ public class DictonaryGrid
 
 
 
-    public void SetPosition(Vector2 pos, byte value)
+    public void SetPosition(Vector2Int pos, byte value)
     {
-        grid.Add(pos, value);
+        grid[pos] = value;
+    }
+
+
+
+    public void UpdatePosition(Vector2Int pos, byte value)
+    {
+        grid[pos] |= value;
+    }
+
+
+
+    public static byte GetPathway(Vector2Int position, Vector2Int direction)
+    {
+        byte pathway = 0;
+
+        return pathway;
     }
 }
