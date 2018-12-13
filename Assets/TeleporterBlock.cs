52bd89d52828907bff7cf9bbb8f6859342083ec9
tylcos <tylcos@users.noreply.github.com>
2018-12-13 07:00:54 -0500

    Started work on spawning teleporter tiles on rooms.

--- Assets/TeleporterBlock.cs
+++ Assets/TeleporterBlock.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 
 
 public class TeleporterBlock : MonoBehaviour 
 {
+    public Transform room;
+    public TileBase tile;
+
     public Transform target;
     public Vector3 offset;
 
+    public bool roomClear = true;
 
 
+
+    private void Start()
+    {
+        Vector3Int position = new Vector3Int(10, 0, 110);
+
+        Tilemap tilemap = room.GetComponent<Tilemap>();
+        tilemap.SetTile(position, tile);
+        tilemap.RefreshTile(position);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
