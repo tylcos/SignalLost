223bd60e5557645ca2e57b50826375ad6e5e515b
Ishaan Sharma <ishaan0sharma@me.com>
2019-01-07 14:45:58 -0500

    Revert "Revert "Added in delay to idle animation for player""
    This reverts commit 23757e1b352077c09a64ab6b30e41e663da63a58.

--- Assets/Scripts/Characters/PlayerAnimation.cs
+++ Assets/Scripts/Characters/PlayerAnimation.cs
@@ -8,59 +8,14 @@ public class PlayerAnimation : MonoBehaviour {
     [HideInInspector]
     public PlayerController playerControllerAccessor;
 
-	// Use this for initialization
-	void Start () {
-        transform.hasChanged = false;
-	}
-
-    /*
+	// Use this for initialization    
         public void Start()
         {
-            StartCoroutine(Example_WaitForSeconds_Coroutine());
-        }
-
-        public IEnumerator Example_WaitForSeconds_Coroutine()
-        {
-            Debug.Log("Start of WaitForSeconds Example");
-
-            yield return new WaitForSeconds(1.5f);
-
-            Debug.Log("End of WaitForSeconds Example");
-        }
-    */
-
-    //TESTING
-    public IEnumerator TestCoroutine()
-    {
-        while (true)
-        {
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A)
-                || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-            {
-                animator.SetBool("Moving", true);
-                if (Input.GetKeyDown(KeyCode.V))
-                {
-                    MeleeAnimation();
-                }
-            }
-            else
-            {
-                yield return new WaitForSeconds(2f);
-                animator.SetBool("Moving", false);
-                transform.hasChanged = false;
-            }
-            MeleeAnimation();
-        }
-    }
-    //END
+            transform.hasChanged = false;
+        }    
 
 	// Update is called once per frame
 	void Update () {
-        //TESTING
-       // StartCoroutine(TestCoroutine());
-        //END
-        
-        /*
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) 
             || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
@@ -72,14 +27,14 @@ public class PlayerAnimation : MonoBehaviour {
         }
         else
         {
-            animator.SetBool("Moving", false);
             transform.hasChanged = false;
-
+            animator.SetBool("Moving", false);
         }
-        MeleeAnimation();*/
-	}
+        MeleeAnimation();
+    }
 
-    
+
+
     private void MeleeAnimation()
     {
         // TODO?: 
