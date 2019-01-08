17df7ebdb8d93213cae1d84a8f3413966df77152
Tyler Cosentino <tylcos@users.noreply.github.com>
2019-01-08 15:27:53 -0500

    Updated cinemachine and made the connectors on a room stored in a single array

--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -3,14 +3,13 @@ using UnityEngine.Tilemaps;
 using System.Linq;
 using UnityEditor;
 
+
+
 public class RoomManager : MonoBehaviour 
 {
     public BoundsInt bounds;
 
-    public int[] bottomConnectors = new int[0];
-    public int[] leftConnectors = new int[0];
-    public int[] topConnectors = new int[0];
-    public int[] rightConnectors = new int[0];
+    public int[][] connectors = { new int[0], new int[0], new int[0], new int[0] };
 
 
 
@@ -36,21 +35,19 @@ public class RoomManager : MonoBehaviour
 
         var minX = bounds.min + new Vector3(1, 0.5f);
         var minY = bounds.min + new Vector3(.5f, 1);
-        foreach (int pos in bottomConnectors) Gizmos.DrawCube(minX + new Vector3Int(pos, 0, 0), new Vector3(2,1));
-        foreach (int pos in topConnectors)    Gizmos.DrawCube(minX + new Vector3Int(pos, bounds.size.y - 1, 0), new Vector3(2, 1));
-        foreach (int pos in leftConnectors)   Gizmos.DrawCube(minY + new Vector3Int(0, pos, 0), new Vector3(1, 2));
-        foreach (int pos in rightConnectors)  Gizmos.DrawCube(minY + new Vector3Int(bounds.size.x - 1, pos, 0), new Vector3(1, 2));
+        foreach (int pos in connectors[0]) Gizmos.DrawCube(minX + new Vector3Int(pos, 0, 0), new Vector3(2,1));
+        foreach (int pos in connectors[1]) Gizmos.DrawCube(minX + new Vector3Int(pos, bounds.size.y - 1, 0), new Vector3(2, 1));
+        foreach (int pos in connectors[2]) Gizmos.DrawCube(minY + new Vector3Int(0, pos, 0), new Vector3(1, 2));
+        foreach (int pos in connectors[3]) Gizmos.DrawCube(minY + new Vector3Int(bounds.size.x - 1, pos, 0), new Vector3(1, 2));
     }
 
     void OnValidate()
     {
-        bottomConnectors = bottomConnectors.Select(c => Mathf.Clamp(c, 0, bounds.size.x - 2)).ToArray();
-        topConnectors = topConnectors.Select(c => Mathf.Clamp(c, 0, bounds.size.x - 2)).ToArray();
-        leftConnectors = leftConnectors.Select(c => Mathf.Clamp(c, 0, bounds.size.y - 2)).ToArray();
-        rightConnectors = rightConnectors.Select(c => Mathf.Clamp(c, 0, bounds.size.y - 2)).ToArray();
+        for (int i = 0; i < 4; i++)
+            connectors[i] = connectors[i].Select(c => Mathf.Clamp(c, 0, bounds.size.x - 2)).ToArray();
     }
-
-    [MenuItem("CONTEXT/RoomManager/Update Bound Size")]
+    
+    [ContextMenu("Update Bound Size")]
     public void UpdateBoundSize()
     {
         OnEnable();
