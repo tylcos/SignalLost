2015a46dd3571a8edfba3b8d13fa1527157e2f63
Allen <loftedspy@gmail.com>
2019-01-09 15:40:47 -0500

    Ammo counter

--- Assets/Scripts/UI/AmmoCounter.cs
+++ Assets/Scripts/UI/AmmoCounter.cs
@@ -6,11 +6,20 @@ using TMPro;
 public class AmmoCounter : MonoBehaviour {
 
     public WeaponManager characterWeapon;
-    public TextMeshPro tmp;
-    
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    private TMP_Text m_text;
+
+    private void Awake()
+    {
+        // Get a reference to the text component.
+        // Since we are using the base class type <TMP_Text> this component could be either a <TextMeshPro> or <TextMeshProUGUI> component.
+        m_text = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        int cur = characterWeapon.CurrentAmmo;
+        int max = characterWeapon.MaxAmmo;
+        m_text.text = cur + " / " + max;
 	}
 }
