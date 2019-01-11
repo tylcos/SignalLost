2dd9c550865971e2bcee9fbc52beb857a69df8f7
Tyler Cosentino <tylcos@users.noreply.github.com>
2019-01-11 15:01:03 -0500

    Started work on custom editor ui

---
+++ Assets/Scripts/EditorUI/RoomManagerEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+
+
+// https://www.youtube.com/watch?v=RInUu1_8aGw
+[CustomEditor(typeof(RoomManager))]
+[CanEditMultipleObjects]
+class RoomManagerEditor : Editor
+{   
+    SerializedProperty connectors;
+
+    void OnEnable() 
+    {
+        connectors = serializedObject.FindProperty("connectors");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        for (int i = 0; i < 4; i++)
+        {
+            EditorGUILayout.PropertyField(connectors.GetArrayElementAtIndex(i), new GUIContent("Lamo"));
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}   
--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -60,22 +60,3 @@ public class RoomManager : MonoBehaviour
         gameObject.SetActive(enabled);
     }
 }
-
-
-
-public class Connector
-{
-    public int[] connections;
-
-    public Connector()
-    {
-        connections = new int[0];
-    }
-
-    public int this [int i]
-    {
-        get { return connections[i]; }
-        set { connections[i] = value; }
-    }
-}
-
