9c30c2b0f1a742e4032fe05acc7d943ec9de7d54
Tyler Cosentino <tylcos@users.noreply.github.com>
2019-01-14 14:33:47 -0500

    Fix old changes

--- Assets/Scripts/EditorUI/RoomManagerEditor.cs
+++ Assets/Scripts/EditorUI/RoomManagerEditor.cs
@@ -7,27 +7,44 @@ using UnityEditor;
 using UnityEngine;
 
 
-
+/*
 // https://www.youtube.com/watch?v=RInUu1_8aGw
 [CustomEditor(typeof(RoomManager))]
 [CanEditMultipleObjects]
 class RoomManagerEditor : Editor
-{   
-    SerializedProperty connectors;
+{
+    int[][] connectors;
+
+    bool showSides = true;
+    bool[] showSide = { true, true, true, true };
+
+
 
     void OnEnable() 
     {
-        connectors = serializedObject.FindProperty("connectors");
+        connectors = ((RoomManager)target).connectors;
     }
 
     public override void OnInspectorGUI()
     {
-        serializedObject.Update();
+        EditorGUILayout.Space();
 
-        for (int i = 0; i < 4; i++)
+        showSides = EditorGUILayout.Foldout(showSides, "Sides");
+        if (showSides)
         {
-            EditorGUILayout.PropertyField(connectors.GetArrayElementAtIndex(i), new GUIContent("Lamo"));
-            serializedObject.ApplyModifiedProperties();
+            EditorGUI.indentLevel++;
+
+            for (ushort s = 0; s < 4; s++)
+            {
+                var side = connectors[s];
+
+                showSide[s] = EditorGUILayout.Foldout(showSide[s], "Side" + s);
+                if (showSide[s])
+                {
+                    side[0] = EditorGUILayout.IntField(side[0], new GUIStyle());
+                }
+
+            }
         }
     }
-}   
+}*/
