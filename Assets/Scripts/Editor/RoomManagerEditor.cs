776811ebf06aa00331237a4bc90d7505b91bd4ea
tylcos <tylcos@users.noreply.github.com>
2019-01-17 06:56:37 -0500

    Completed work on custom inspector for rooms

---
+++ Assets/Scripts/Editor/RoomManagerEditor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+
+[CustomEditor(typeof(RoomManager))]
+[CanEditMultipleObjects]
+class RoomManagerEditor : Editor
+{
+    public List<int>[] connectors = new List<int>[4];
+    public BoundsInt bounds;
+
+
+
+    private RoomManager castedTarget;
+    private readonly GUILayoutOption width = GUILayout.Width(25);
+
+
+
+    void OnEnable()
+    {
+        castedTarget = (target as RoomManager);
+
+        bounds = serializedObject.FindProperty("bounds").boundsIntValue;
+
+        // Necessary because unity cannot serialize an array of lists
+        connectors[0] = castedTarget.side1;
+        connectors[1] = castedTarget.side2;
+        connectors[2] = castedTarget.side3;
+        connectors[3] = castedTarget.side4;
+    }
+
+
+
+    public override void OnInspectorGUI()
+    {
+        GUI.skin.button.alignment = TextAnchor.MiddleCenter;
+        GUI.skin.button.margin = new RectOffset(5, 5, 0, 0);
+
+        for (int r = 0; r < 4; r++)
+        {
+            var currentRow = connectors[r];
+
+            EditorGUILayout.BeginHorizontal();
+            
+            if (GUILayout.Button("+", width))
+            {
+                Undo.RecordObject(castedTarget, "Change pathways");
+                currentRow.Add(0);
+                UpdateValues();
+            }
+                
+
+            if (GUILayout.Button("-", width) && currentRow.Count > 0)
+            {
+                Undo.RecordObject(castedTarget, "Change pathways");
+                currentRow.RemoveAt(currentRow.Count - 1);
+                UpdateValues();
+            }
+
+
+            for (int c = 0; c < currentRow.Count; c++)
+            {
+                int size = r < 2 ? bounds.size.x : bounds.size.y;
+                int changedValue = EditorGUILayout.IntSlider(currentRow[c], 0, size - 2);
+
+                if (changedValue != currentRow[c])
+                {
+                    Undo.RecordObject(castedTarget, "Change pathways");
+                    currentRow[c] = changedValue;
+                    UpdateValues();
+                }
+            }
+
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    private void UpdateValues()
+    {
+        castedTarget.side1 = connectors[0];
+        castedTarget.side2 = connectors[1];
+        castedTarget.side3 = connectors[2];
+        castedTarget.side4 = connectors[3];
+
+        castedTarget.UpdateConnectors();
+    }
+}
--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Linq;
-using UnityEditor;
+using System.Collections.Generic;
 
 
 
 public class RoomManager : MonoBehaviour 
 {
-    public BoundsInt bounds;
+    // Necessary because unity cannot serialize an array of lists
+    public List<int>[] connectors = new List<int>[4];
+    public List<int> side1 = new List<int>(); public List<int> side2 = new List<int>();
+    public List<int> side3 = new List<int>(); public List<int> side4 = new List<int>();
 
-    public Side[] sides = { new Side(), new Side(), new Side(), new Side() };
+    public BoundsInt bounds;
 
 
 
     private void OnEnable()
     {
+        // Update bounds
         var roomBounds = gameObject.GetComponent<Tilemap>().localBounds;
         var min = roomBounds.min + transform.position;
         var size = roomBounds.size;
@@ -25,27 +29,26 @@ public class RoomManager : MonoBehaviour
     }
 
 
+
     private bool gizmosEnabled;
     void OnDrawGizmos()
-    {   
+    {
+        UpdateConnectors();
         if (!gizmosEnabled)
             OnEnable();
+        
 
         Gizmos.color = Color.magenta;
 
         var minX = bounds.min + new Vector3(1, 0.5f);
         var minY = bounds.min + new Vector3(0.5f, 1);
-        foreach (int pos in sides[0].connections) Gizmos.DrawCube(minX + new Vector3Int(pos, 0, 0), new Vector3(2,1));
-        foreach (int pos in sides[1].connections) Gizmos.DrawCube(minX + new Vector3Int(pos, bounds.size.y - 1, 0), new Vector3(2, 1));
-        foreach (int pos in sides[2].connections) Gizmos.DrawCube(minY + new Vector3Int(0, pos, 0), new Vector3(1, 2));
-        foreach (int pos in sides[3].connections) Gizmos.DrawCube(minY + new Vector3Int(bounds.size.x - 1, pos, 0), new Vector3(1, 2));
+        foreach (int pos in connectors[0]) Gizmos.DrawCube(minX + new Vector3Int(pos, 0, 0), new Vector3(2,1));
+        foreach (int pos in connectors[1]) Gizmos.DrawCube(minX + new Vector3Int(pos, bounds.size.y - 1, 0), new Vector3(2, 1));
+        foreach (int pos in connectors[2]) Gizmos.DrawCube(minY + new Vector3Int(0, pos, 0), new Vector3(1, 2));
+        foreach (int pos in connectors[3]) Gizmos.DrawCube(minY + new Vector3Int(bounds.size.x - 1, pos, 0), new Vector3(1, 2));
     }
 
-    void OnValidate()
-    {
-        for (int i = 0; i < 4; i++)
-            sides[i].connections = sides[i].connections.Select(c => Mathf.Clamp(c, 0, bounds.size.x - 2)).ToArray();
-    }
+
     
     [ContextMenu("Update Bound Size")]
     public void UpdateBoundSize()
@@ -59,17 +62,13 @@ public class RoomManager : MonoBehaviour
     {
         gameObject.SetActive(enabled);
     }
-}
 
-
-
-[System.Serializable]
-public class Side
-{
-    public int[] connections;
-
-    public Side()
+    public void UpdateConnectors()
     {
-        connections = new int[0];
+        // Update connectors
+        connectors[0] = side1;
+        connectors[1] = side2;
+        connectors[2] = side3;
+        connectors[3] = side4;
     }
 }
