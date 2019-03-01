22e9466d771ec24d8295379b27e00c5dc56b4e28
Allen <loftedspy@gmail.com>
2019-03-01 15:21:19 -0500

    enemies can use weaponV2

--- Assets/Scripts/Weapons/WeaponV2/PlayerWeaponController.cs
+++ Assets/Scripts/Weapons/WeaponV2/PlayerWeaponController.cs
@@ -4,48 +4,10 @@ using UnityEngine;
 
 public class PlayerWeaponController : WeaponController
 {
-    private const int INVSIZE = 4;
-    [SerializeField]
-    public WeaponV2Information[] inventory = new WeaponV2Information[INVSIZE];
-    private GameController master;
-    private List<EquippedWeapon> swapList = new List<EquippedWeapon>(INVSIZE);
-    private int swapListIndex = 0;
-
     public delegate void PWCReloadHandler(bool reloading, float progress);
     public event PWCReloadHandler ReloadUpdate;
     public delegate void PWCFireError();
     public event PWCFireError FireError;
-
-    private void OnValidate()
-    {
-        if(inventory.Length != INVSIZE)
-        {
-            Debug.LogWarning("Don't change the weapon inventory array size!");
-            System.Array.Resize(ref inventory, INVSIZE);
-        }
-    }
-
-    private void OnEnable()
-    {
-        master = GameObject.FindGameObjectWithTag("Master").GetComponent<GameController>();
-        // populates the swaplist
-        foreach (WeaponV2Information wep in inventory)
-        {
-            // create a class that stores a reference to the object we 
-            if(wep == null)
-                swapList.Add(EquippedWeapon.empty);
-            else
-                swapList.Add(new EquippedWeapon(wep, transform, bulletLayer));
-            // so we have a parallel array that holds the objects, their bullets, and ammo
-            // so we call commands there when we need to fire or whatnot since that stores everything
-        }
-        swapList[0].SetEnabled(true);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
@@ -117,10 +79,5 @@ public class PlayerWeaponController : WeaponController
             // also put a bar above the ammo counter that fills up with respect to the reloadProgress
             // use events to do update the UI btw
         }
-    }
-
-    public EquippedWeapon GetEquippedWeapon()
-    {
-        return swapList[swapListIndex];
     }
 }
