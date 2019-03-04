c979180013169e69e33036300fbfe5bb596b8feb
Ishaan Sharma <ishaan0sharma@me.com>
2019-03-04 16:26:35 -0500

    Tilemaps for all (except one) room have been changed

--- Assets/Scripts/Characters/EnemyAnimation.cs
+++ Assets/Scripts/Characters/EnemyAnimation.cs
@@ -2,72 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAnimation : MonoBehaviour {
+public class EnemyAnimation : MonoBehaviour
+{
 
-    public Animator animator;
-    [HideInInspector]
-    public GameObject meleeEnemy;
-    [HideInInspector]
-    public EnemyController enemyControllerAccessor;
-    [HideInInspector]
-    public bool formation = false;
-    [HideInInspector]
-    public bool inRange = false;
+    private RangedEnemyController rangedEnemyController;
+    public Animator enemyAnimator;
 
     // Use this for initialization
-    void Start () {
-        enemyControllerAccessor = meleeEnemy.GetComponent<EnemyController>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        // May use if statements to call specific methods for specific enemies
-        // OR May make seperate scripts for various types of enemies
-        // OR May seperate if statements into own methods to call in enemy type animations
-        RangedEnemyAnim();
-    }
-
-    //Enemy Type Animations
-    private void RangedEnemyAnim()
+    void Start()
     {
-        if (enemyControllerAccessor.isAggro == true)
-        {
-            animator.SetBool("PlayerInRange", true);
-            inRange = true;
-        }
-        else
-        {
-            animator.SetBool("PlayerInRange", false);
-            inRange = false;
-        }
-
-        if (inRange)
-        {
-            animator.SetBool("Formation", true);
-            formation = true;
-        }
-        else
-        {
-            animator.SetBool("Formation", false);
-            formation = false;
-        }
-        /*if (formation)
-        {
-            animator.SetBool("Attack", true);
-            if (enemyControllerAccessor.IsAggro == false)
-            {
-                animator.SetBool("PlayerInRange", false);
-                inRange = false;
-                formation = false;
-            }
-        }*/
+        rangedEnemyController = this.gameObject.GetComponent<RangedEnemyController>();
     }
- 
-    /* private void MeleeEnemyAnim(){
-     
-    }*/
 
-    /*private void BossAnim(){
-    
-    }*/
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (rangedEnemyController.movingForAnimation)
+        {
+            
+        }
+    }
+
 }
