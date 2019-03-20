c81b264e4c0147688d2937ccb7d0835427003925
Tyler Cosentino <tylcos@users.noreply.github.com>
2019-03-20 15:00:57 -0400

    More work on the time score and loading the death scene and start scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,24 +6,29 @@ using UnityEngine.SceneManagement;
 
 public static class LevelManager
 {
-    public static string levelName = "Release";
+    public static string gameSceneName = "Release";
+    public static string startSceneName = "Menu";
 
 
-    
-    public static int startingLevels = 6;
+
+    public static int startingRooms = 6;
     public static int currentLevel = 0;
+
+
 
     public static long timeAtLevelLoad;
 
     private static readonly int baseTimeScore = 500;
-    private static readonly int timeExpectedPerLevel = 15;
-    private static readonly int timeMaxPerLevel = 30;
+    private static readonly int timeExpectedPerRoom = 15;
+    private static readonly int timeExpectedPerBoss = 30;
+    private static readonly int timeMaxPerRoom = 30;
+    private static readonly int timeMaxPerBoss = 60;
 
 
 
     public static void LoadStartingLevel()
     {
-        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene(gameSceneName);
 
         timeAtLevelLoad = DateTime.UtcNow.Ticks;
     }
@@ -38,33 +43,34 @@ public static class LevelManager
 
 
         ++currentLevel;
-        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene(gameSceneName);
         // Spawn rooms with LevelsToSpawn
         // Remove blind from camera
     }
 
-    public static void LoadDeathScene()
+    public static void LoadDeathScreen()
     {
-
+        // Load death ui not a scene
     }
 
+    // Selected at death screen
     public static void LoadStartScene()
     {
-
+        SceneManager.LoadScene(startSceneName);
     }
 
 
 
     private static int ScoreFromTime(long timeTaken)
     {
-        long timeExpected = timeExpectedPerLevel * LevelsToSpawn;
-        long timeMax = timeMaxPerLevel * LevelsToSpawn - timeExpected;
+        long timeExpected = timeExpectedPerRoom * LevelsToSpawn + timeExpectedPerBoss;
+        long timeMax = timeMaxPerRoom * LevelsToSpawn + timeMaxPerBoss;
 
         if (timeTaken <= timeExpected)
             return baseTimeScore;
         else
-            return (int)(baseTimeScore * (double)(timeTaken - timeExpected) / timeMax);
+            return (int)(baseTimeScore * (double)(timeMax - timeTaken) / (timeMax - timeExpected));
     }
 
-    private static int LevelsToSpawn => startingLevels + currentLevel * 2;
+    private static int LevelsToSpawn => startingRooms + currentLevel * 2;
 }
